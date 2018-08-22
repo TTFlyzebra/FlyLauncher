4ddb10bf530a11dcd1a9765977cd6f95e4fdca74
flyzebra <852184385@qq.com>
2018-08-22 14:39:21 +0800

    Initial commit

---
+++ Launcher/src/main/java/com/jancar/launcher/view/FlyViewPager.java
@@ -0,0 +1,15 @@
+package com.jancar.launcher.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+public class FlyViewPager extends ViewPager{
+    public FlyViewPager(Context context) {
+        super(context);
+    }
+
+    public FlyViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ Launcher/src/main/java/com/jancar/launcher/view/OnePagerView.java
@@ -0,0 +1,19 @@
+package com.jancar.launcher.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+public class OnePagerView extends FrameLayout{
+    public OnePagerView( Context context) {
+        super(context);
+    }
+
+    public OnePagerView( Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public OnePagerView( Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
